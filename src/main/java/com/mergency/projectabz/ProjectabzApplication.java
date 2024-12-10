274963d80fdaf809c11b84fb8aba1e0e4eef28ee
Bank <blankinfear@gmail.com>
2024-12-10 13:58:27 +0700

    .env and check status of server

--- src/main/java/com/mergency/projectabz/ProjectabzApplication.java
+++ src/main/java/com/mergency/projectabz/ProjectabzApplication.java
@@ -1,9 +1,13 @@
 package com.mergency.projectabz;
 
+import java.sql.DriverManager;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.sql.Connection;
 
 @SpringBootApplication
 public class ProjectabzApplication {
@@ -15,6 +19,22 @@ public class ProjectabzApplication {
 
 	@Bean
 	public CommandLineRunner commandLineRunner() {
-		return args -> System.out.println("Connection Complete");
+		return args -> {
+			try {
+				String url = System.getProperty("DB_URL");
+				String username = System.getProperty("DB_USERNAME");
+				String password = System.getProperty("DB_PASSWORD");
+
+				try (Connection connection = DriverManager.getConnection(url, username, password)) {
+					if (connection != null) {
+						System.out.println("Connection Complete");
+					} else {
+						throw new Exception("Connection Failed");
+					}
+				}
+			} catch (Exception e) {
+				System.err.println("Error: " + e.getMessage());
+			}
+		};
 	}
 }
